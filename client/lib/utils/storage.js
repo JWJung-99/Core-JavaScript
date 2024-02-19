160777523ab9a7fef5a9f338499a142062854226
JWJung-99 <39busy@naver.com>
2024-02-19 14:32:33 +0900

    localStorage: final
    - getStorage
    - setStorage
    - deleteStorage

--- client/lib/utils/storage.js
+++ client/lib/utils/storage.js
@@ -1,3 +1,66 @@
 
+import { isString } from "./typeOf.js";
 
-console.log('storage');
+
+// setStorage()
+
+const {localStorage: storage} = window;
+
+export function setStorage(key, value) {
+
+  return new Promise((resolve, reject) => {
+
+    if (isString(key)) {
+      storage.setItem(key, JSON.stringify(value));
+      resolve();
+    } else {
+      reject({message : 'key는 문자 타입이어야 합니다!'});
+    }
+
+    
+  })
+}
+
+export function getStorage(key) {
+  return new Promise((resolve, reject) => {
+    if (isString(key)) {
+      resolve(JSON.parse(storage.getItem(key)));
+    } else {
+      reject({message : 'key는 문자 타입이어야 합니다!'});
+    }
+  })
+}
+
+
+
+export function deleteStorage(key) {
+  return new Promise((resolve, reject) => {
+    !key ? storage.clear() : storage.removeItem(key);
+    resolve();
+  })
+}
+
+
+
+// await setStorage('user', {name: 'tiger'});
+
+// const data = await getStorage('user');
+// console.log(data);
+
+// deleteStorage('user');
+// deleteStorage();
+
+
+
+
+
+
+
+
+
+
+
+// localStorage.setItem('user', JSON.stringify({name: 'tiger', age: 15}));
+
+// console.log(JSON.parse(storage.getItem('user')));
+
--- client/main.js
+++ client/main.js
@@ -1,2 +1,53 @@
 
-import { } from './lib/index.js';
+import { 
+  getNode,
+  setStorage,
+  getStorage,
+  deleteStorage,
+  clearContents,
+} from './lib/index.js';
+
+
+
+// 1. textField의 값을 localStorage에 저장
+// 2. 페이지 로딩 시 바로 실행되는 함수 만들기 (IIFE)
+// 3. 스토리지 값을 가져와 textField에 렌더링하기
+
+
+
+const textField = getNode('#textField');
+const clear = getNode('[data-name="clear"]');
+
+
+const handleTextField = (e) => {
+  const value = e.currentTarget.value;
+  setStorage('text', value);
+}
+
+
+const handleClear = () => deleteStorage('text').then(clearContents(textField));
+
+
+textField.addEventListener('input', handleTextField);
+clear.addEventListener('click', handleClear);
+
+
+// 페이지 새로고침 할 때마다 실행
+// function init() {
+//   console.log('init');
+// }
+// window.addEventListener('DOMContentLoaded', init);
+
+// IIFE, then
+// (() => {
+//   getStorage('text').then((res) => {
+//     textField.value = res;
+//   });
+// })();
+
+// IIAFE
+(async() => {
+  const data = await getStorage('text');
+
+  textField.value = data;
+})();
